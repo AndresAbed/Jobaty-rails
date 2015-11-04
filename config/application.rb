adf01adc8acfcdbffc448d20ab16bedae4676170
AndresAbed <abedandres@gmail.com>
2015-11-04 18:14:12 -0300

    Creates project
    Changes time zone and default language
    Configures pg gem and creates the dbs
    Sort gemfile lines

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Jobaty
+  class Application < Rails::Application
+    config.i18n.default_locale = :es
+    config.time_zone = 'Buenos Aires' 
+    config.active_record.default_timezone = :local
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
