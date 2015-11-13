e52eba58efeb7e88f4ddda735a6dda68019be575
AndresAbed <abedandres@gmail.com>
2015-11-13 14:01:18 -0300

    Fixes syntax error

--- db/migrate/20151111221837_adds_omniauth_columns_to_employees.rb
+++ db/migrate/20151111221837_adds_omniauth_columns_to_employees.rb
@@ -2,6 +2,6 @@ class AddsOmniauthColumnsToEmployees < ActiveRecord::Migration
   def change
     add_column :employees, :provider, :string
     add_column :employees, :uid, :string
-    add_column :employees, :oauth_token :string
+    add_column :employees, :oauth_token, :string
   end
 end
