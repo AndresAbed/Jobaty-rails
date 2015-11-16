5908f93594d6c46426ab9b23c4793febbdc690ed
AndresAbed <abedandres@gmail.com>
2015-11-16 18:48:16 -0300

    Adds profile image to Company

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113205300) do
+ActiveRecord::Schema.define(version: 20151116204551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                    default: "", null: false
+    t.string   "encrypted_password",       default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "name"
     t.string   "address"
     t.string   "city"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 20151113205300) do
     t.string   "linkedin"
     t.string   "twitter"
     t.string   "other_web"
+    t.string   "profile_img_file_name"
+    t.string   "profile_img_content_type"
+    t.integer  "profile_img_file_size"
+    t.datetime "profile_img_updated_at"
   end
 
   add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
