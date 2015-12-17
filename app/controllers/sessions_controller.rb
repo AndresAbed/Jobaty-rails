f2468d8dd217929509d5e1ec52c7797d36183110
AndresAbed <abedandres@gmail.com>
2015-12-17 16:57:17 -0300

    Removes employees and companies folders in controllers
    Removes companies method for main controller
    Adds after sign out path method in application controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,53 @@
+class SessionsController < DeviseController
+  prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
+  prepend_before_filter :allow_params_authentication!, :only => :create
+  prepend_before_filter { request.env["devise.skip_timeout"] = true }
+
+  prepend_view_path 'app/views/devise'
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate(auth_options)
+    if resource && resource.active_for_authentication?
+      sign_in(resource_name, resource)
+      @flag = true
+      respond_to do |format|
+        format.js
+      end
+    else
+      respond_to do |format|
+          format.js
+      end
+    end
+  end
+
+  def destroy
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
+  end
+
+  protected
+
+  def sign_in_params
+    devise_parameter_sanitizer.sanitize(:sign_in)
+  end
+
+  def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    { :methods => methods, :only => [:password] }
+  end
+
+  def auth_options
+    { :scope => resource_name, :recall => "#{controller_path}#new" }
+  end
+end
