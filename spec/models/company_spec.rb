810d09eb8257a06f98063021450d2ece417a40f4
AndresAbed <abedandres@gmail.com>
2015-12-21 00:14:06 -0300

    Adds company model spec

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  it 'has a valid factory' do
+    expect(build(:company)).to be_valid
+  end
+  it 'is invalid without a name' do
+    expect(build(:company, name: nil)).to_not be_valid
+  end
+end
