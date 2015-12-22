52822e2e7df6c374c8a39589f113646b1c29e1f0
AndresAbed <abedandres@gmail.com>
2015-12-22 17:23:05 -0300

    Changes update method for passwords controller
    Modifies passwords -> create.js

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,6 +14,20 @@ class PasswordsController < Devise::PasswordsController
       end
     end
   end
+
+  def update
+    self.resource = resource_class.reset_password_by_token(resource_params)
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      redirect_to root_path
+      flash[:notice] = "Tu contraseña fue actualizada"
+    else
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
   protected
   def after_sending_reset_password_instructions_path_for(resource_name)
     case resource_name
